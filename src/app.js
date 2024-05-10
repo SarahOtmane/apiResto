3fae751fad2713c81147dfeda9915c69678904a8
SarahOtmane <sarahotmane02@gmail.com>
2024-05-10 11:25:46 +0200

    creation du fichier app.js config de la bdd lien vers la doc swagger

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const sequelize = require("sequelize");
+
+const app = express();
+const port = 3003;
+
+
+// Configuration de la base de données
+const db = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: "db",
+    dialect: "mysql"
+});
+
+// Test de la connexion à la base de données
+db.authenticate()
+    .then(() => {
+        console.log("Connecté à la base de données MySQL!");
+    })
+    .catch(err => {
+        console.error("Impossible de se connecter à la base de données:", err);
+});
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+
+
+// Configuration de Swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./docs/config.js');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+// Démarrage du serveur
+app.listen(port, () => {
+    console.log(`L'application écoute sur le port ${port}`);
+});
+
+
