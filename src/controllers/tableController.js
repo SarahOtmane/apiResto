4500b07937dbf2afeafaec8e2648b5d3487df0ee
SarahOtmane <sarahotmane02@gmail.com>
2024-05-13 17:09:48 +0200

    updateTable

--- src/controllers/tableController.js
+++ src/controllers/tableController.js
@@ -44,3 +44,57 @@ exports.createTable = async(req, res) =>{
         res.status(500).json({message: "Erreur lors du traitement des données."});
     }
 }
+
+
+
+
+
+/**********************************************************
+            MÉTHODE POUR MODIFIER UNE TABLE
+**********************************************************/
+/*
+    Fonction qui permet de modifier une table
+
+    Les vérifications : 
+        - l existance du plan de table
+        - l existance du numero de table dans le plan
+
+*/
+exports.updateTable = async(req, res) =>{
+    try {
+        const existingPlan = await PlanTable.findOne({ where: { id: req.params.id_planTable } });
+        if (!existingPlan) {
+            return res.status(404).json({ message: 'Le plan de table nexiste pas.' });
+        }
+        
+        const existingTable = await Table.findOne({where: {
+            id_planTable: req.params.id_planTable,
+            id_table: req.params.id_table,
+        }});
+        if (existingTable) {
+            return res.status(401).json({ message: 'La table existe pas.' });
+        }
+
+        const table = await Table.findOne({where: {
+            id_planTable: req.params.id_planTable,
+            numero: req.body.numero,
+        }});
+        if (table) {
+            return res.status(401).json({ message: 'Une table portant ce numero existe déja' });
+        }
+        
+        await table.update({
+            id_planTable: req.params.id_planTable,
+            numero: req.body.numero,
+            nbPlaces: req.body.nbPlaces,
+            taken: false,
+        });
+
+        res.status(201).json({ 
+            message: `Table mis à jour avec succès` 
+        });
+
+    } catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+}
