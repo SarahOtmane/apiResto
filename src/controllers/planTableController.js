3958e7c42e6e321e8800843f359186cab5e35bd8
SarahOtmane <sarahotmane02@gmail.com>
2024-05-13 15:28:27 +0200

    updatePlanTable

--- src/controllers/planTableController.js
+++ src/controllers/planTableController.js
@@ -31,8 +31,8 @@ exports.createPlanTable = async(req, res) =>{
             id_resto: req.params.id_resto,
             name: req.body.name,
         }});
-        if (!existingPlan) {
-            return res.status(401).json({ message: 'Le plan de travail correpondant à cette salle existe déja.' });
+        if (existingPlan) {
+            return res.status(401).json({ message: 'Le plan de table correpondant à cette salle existe déja.' });
         }
 
         const planTable = await PlanTable.create({
@@ -45,6 +45,58 @@ exports.createPlanTable = async(req, res) =>{
 
         res.status(201).json({ 
             message: `Plan de table crée avec succès` 
+        });
+
+    } catch (error) {
+        res.status(500).json({message: "Erreur lors du traitement des données."});
+    }
+}
+
+
+/**********************************************************
+            MÉTHODE POUR MODIFIER UN PLAN DE TABLE
+**********************************************************/
+/*
+    Fonction qui permet de modifier un plan de table
+
+    Les vérifications : 
+        - l existance du resto
+        - l existance du plan de travail
+        - il n ya pas de plan de travail qui correspond a cette salle
+
+*/
+exports.updatePlanTable = async(req, res) =>{
+    try {
+        const existingResto = await Resto.findOne({ where: { id: req.params.id_resto } });
+        if (!existingResto) {
+            return res.status(404).json({ message: 'Ce resto nexiste pas.' });
+        }
+
+        const existingPlan = await PlanTable.findOne({where: {
+            id_resto: req.params.id_resto,
+            id: req.params.id_planTable,
+        }});
+        if (!existingPlan) {
+            return res.status(401).json({ message: 'Le plan de table n existe pas.' });
+        }
+
+        const planTable = await PlanTable.findOne({where: {
+            id_resto: req.params.id_resto,
+            name: req.body.name,
+        }});
+        if (planTable) {
+            return res.status(401).json({ message: 'Le plan de table correpondant à cette salle existe déja.' });
+        }
+        
+        await planTable.update({
+            name: req.body.name,
+            nbTables: req.body.nbTables,
+            nbPlaces: req.body.nbPlaces,
+            full: false,
+        });
+
+        res.status(201).json({ 
+            message: `Plan de table mis à jour avec succès` 
         });
 
     } catch (error) {
