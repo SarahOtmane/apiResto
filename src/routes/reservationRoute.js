30995a7d6b71f9ecdd8c247694aa98c0ff2d2ce9
SarahOtmane <sarahotmane02@gmail.com>
2024-05-13 17:38:28 +0200

    import necessaire pour les routes

---
+++ src/routes/reservationRoute.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+const reservationController = require('../controllers/reservationController');
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
