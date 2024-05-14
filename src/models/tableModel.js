713a135a263be9e5d7cbecf2a877fe5cb764539d
SarahOtmane <sarahotmane02@gmail.com>
2024-05-14 13:20:19 +0200

    modif model

--- src/models/tableModel.js
+++ src/models/tableModel.js
@@ -24,7 +24,7 @@ const Table = sequelize.define('Table', {
         type: DataTypes.BOOLEAN,
         allowNull: false,
     },
-    id_plantable: {
+    id_planTable: {
         type: DataTypes.INTEGER,
         allowNull: false,
     }
