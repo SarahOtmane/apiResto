1b4cdf4f0186fd5de56c6cf27013c960ba4c8bfd
SarahOtmane <sarahotmane02@gmail.com>
2024-05-14 11:51:27 +0200

    createResa => créer une reservation

--- src/controllers/reservationController.js
+++ src/controllers/reservationController.js
@@ -1,3 +1,22 @@
 const Reservation = require('../models/reservationModel');
 const Resto = require('../models/restoModel');
 const PlanTable = require('../models/planTableModel');
+
+
+
+/**********************************************************
+            MÉTHODE POUR CRÉER UNE RESERVATION
+**********************************************************/
+/*
+    Fonction qui permet à une personne de créer une reservation
+
+    Les vérifications : 
+        - Vérifier que le resto en question existe toujours
+        - Vérifier que le nombre de places libres > nb couverts réservés
+        - Vérifier qu'il y ait des tables qu'on peut groupés pour faire le nb de couverts
+
+*/
+
+exports.createResa = async() =>{
+    
+}
