8dc96ec7f47a9bbc619714ff0eac1636568c845b
SarahOtmane <sarahotmane02@gmail.com>
2024-05-17 14:39:43 +0200

    createResa

--- src/controllers/reservationController.js
+++ src/controllers/reservationController.js
@@ -1,6 +1,7 @@
 const Reservation = require('../models/reservationModel');
 const Resto = require('../models/restoModel');
 const PlanTable = require('../models/planTableModel');
+const Table = require('../models/tableModel');
 
 
 
@@ -25,17 +26,51 @@ const PlanTable = require('../models/planTableModel');
 
 exports.createResa = async(req, res) =>{
     try {
+        //recup le resto en question
         const existingResto = await Resto.findOne({ where: { id: req.params.id_resto } });
         if (!existingResto) {
             return res.status(404).json({ message: 'Ce resto nexiste pas.' });
         }
 
-        const existingPlanTable = await PlanTable.findAll({where: {id_resto: req.params.id_resto}});
+        //recup tous les plan de tables du resto qui ont encore des places libres
+        const existingPlanTable = await PlanTable.findAll({where: {id_resto: req.params.id_resto, full: false}});
         if (!existingPlanTable) {
             return res.status(404).json({ message: 'Aucun plan de table pour ce restaurant.' });
         }
 
-        const salleLibre = [];
+        let availablePlanTables =[];
+        //Pour chaque plan de table
+            //Recup les tables libres
+            //vérifier que le nb de place libres de la salle > nb couverts reserves
+            //Parcourir toutes les tables pour voir s'il ya une table qui peut etre reserve
+        async function getTable(){
+            for (const planTable of existingPlanTables){
+                const tables = await Table.findAll({ where: { id_planTable: planTable.id, taken: false } });
+
+                //Recup le nb de places libres dans la salle
+                let nbPlacesLibres = 0;
+                tables.forEach(table => nbPlacesLibres += parseInt(table.nbPlace));
+
+                //si le nb de places libres > nb de couverts reserve
+                let tableAReserve = [];
+                if(nbPlacesLibres >= req.body.nbPlace){
+                    availablePlanTables.push(planTable);
+                    //recup toutes les tables qui peuvent être reserve
+                    for(const table of tables){
+                        if(table.nbPlace >= req.body.nbPlace){
+                            tableAReserve.push(table);
+                        }
+                    }
+
+                    //si aucune table peut être reservée => voir si on peut coller des tables
+                    if(tableAReserve.length === 0){
+                        
+                    }
+                }
+            }
+        }
+
+        getTable();
 
     } catch (error) {
         res.status(500).json({message: "Erreur lors du traitement des données."});
