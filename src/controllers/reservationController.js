96061d7a2fb8e2a52f5b0be91358f39dc0541d88
SarahOtmane <sarahotmane02@gmail.com>
2024-05-22 16:57:27 +0200

    createResa

--- src/controllers/reservationController.js
+++ src/controllers/reservationController.js
@@ -13,15 +13,9 @@ const Table = require('../models/tableModel');
 
     Les vérifications : 
         - Vérifier que le resto en question existe toujours
-        - Vérifier que le nombre de places libres > nb couverts réservés
-        - Vérifier qu'il y ait des tables qu'on peut groupés pour faire le nb de couverts
-
-    Les étapes :
-        - Récup les détails du resto
-        - Récup les plans de tables du resto
-        - Pour chaque plan de table récup les tables qui sont pas réservées
-        - Véerifier que le nb de places libres > nb couverts
-        - Voir si on peut coller des tables pour faire le nb de couverts
+        - Vérifier que le plan de table existe
+        - Verifier que le plan de table n est pas full
+        - Vérifier que le nombre de places libres > au nombre de couverts reserves
 */
 
 exports.createResa = async(req, res) =>{
@@ -32,45 +26,106 @@ exports.createResa = async(req, res) =>{
             return res.status(404).json({ message: 'Ce resto nexiste pas.' });
         }
 
-        //recup tous les plan de tables du resto qui ont encore des places libres
-        const existingPlanTable = await PlanTable.findAll({where: {id_resto: req.params.id_resto, full: false}});
+        //recup le plan de tables du resto et verifier qu il n est pas full
+        const existingPlanTable = await PlanTable.findOne({where: {id_resto: req.params.id_resto, id: req.params.id_plantable}});
         if (!existingPlanTable) {
-            return res.status(404).json({ message: 'Aucun plan de table pour ce restaurant.' });
+            return res.status(404).json({ message: 'Plan de table inexistant' });
+        }else if(existingPlanTable.full === true){
+            return res.status(404).json({ message: 'Aucune place libre' });
         }
 
-        let availablePlanTables =[];
-        //Pour chaque plan de table
-            //Recup les tables libres
-            //vérifier que le nb de place libres de la salle > nb couverts reserves
-            //Parcourir toutes les tables pour voir s'il ya une table qui peut etre reserve
-        async function getTable(){
-            for (const planTable of existingPlanTables){
-                const tables = await Table.findAll({ where: { id_planTable: planTable.id, taken: false } });
+        const tables = await Table.findAll({ where: { id_planTable: req.params.id_plantable, taken: false } });
 
-                //Recup le nb de places libres dans la salle
-                let nbPlacesLibres = 0;
-                tables.forEach(table => nbPlacesLibres += parseInt(table.nbPlace));
+        //Recup le nb de places libres dans la salle
+        let nbPlacesLibres = 0;
+        tables.forEach(table => nbPlacesLibres += parseInt(table.nbPlace));
 
-                //si le nb de places libres > nb de couverts reserve
-                let tableAReserve = [];
-                if(nbPlacesLibres >= req.body.nbPlace){
-                    availablePlanTables.push(planTable);
-                    //recup toutes les tables qui peuvent être reserve
-                    for(const table of tables){
-                        if(table.nbPlace >= req.body.nbPlace){
-                            tableAReserve.push(table);
-                        }
-                    }
+        //si le nb de places libres < nb de couverts reserve
+        if(nbPlacesLibres < req.body.nbPlace){
+            res.status(401).json({message: "Le nombre de places libres est inférieur au nombre de couverts reservés"});
+        }
 
-                    //si aucune table peut être reservée => voir si on peut coller des tables
-                    if(tableAReserve.length === 0){
-                        
-                    }
-                }
+        let tableAReserve = [];
+                
+        //recup toutes les tables qui peuvent être reserve
+        for(const table of tables){
+            if(table.nbPlace >= req.body.nbPlace){
+                tableAReserve.push(table);
             }
         }
 
-        getTable();
+        //si aucune table peut être reservée => voir si on peut coller des tables
+            // exemple : [4, 4, 2, 2]
+        if(tableAReserve.length === 0){
+            tables.sort((a, b) => b.nbPlace - a.nbPlace); 
+
+            let tab = [tables[0]];
+            if( (tables[0].nbPlace + tables[tables.length-1].nbPlace) >= req.body.nbPlace){
+                tab.push(tables[tables.length-1]);
+            }
+            else{
+                let i = 1;
+                let nb = parseInt(tables[0].nbPlace);
+                while ((i<tables.length) && (nb<req.body.nbPlace)) {
+                    tab.push(tables[i]);
+                    nb += parseInt(tables[i].nbPlace);
+                    i++;
+                };
+            }
+
+            let numTable = `${tab[0].numero}`;
+
+            for ( i = 0; i < tab.length; i++) {
+                await tab[i].update({
+                    taken: true,
+                });
+                
+                numTable += '.' + existingTable.numero
+            }
+
+            let reservation = await Reservation.create({
+                email: req.body.email,
+                phone: req.body.phone,
+                name: req.body.name,
+                date: req.body.date,
+                heure: req.body.heure,
+                nbPlace : req.body.nbPlace,
+                numTable : numTable,
+                id_resto: req.params.id_resto,
+                id_planTable: req.params.id_plantable,
+    
+            });     
+            res.status(201).json(`Reservation réussie : ${reservation.email}`);    
+        }
+
+        //si ya des tables qu'on peut reservé
+            // Ranger le tab tableAReserve par ordre croissant du nbPlaces
+            // creer la reservation avec la premiere table
+        if(tableAReserve.length > 1) tableAReserve.sort((a, b) => a.nbPlace - b.nbPlace);
+
+        const existingTable = await Table.findOne({where: {
+            id_planTable: req.params.id_plantable,
+            numero: tableAReserve[0].numero,
+        }});
+
+        await existingTable.update({
+            taken: true,
+        });
+
+        let reservation = await Reservation.create({
+            email: req.body.email,
+            phone: req.body.phone,
+            name: req.body.name,
+            date: req.body.date,
+            heure: req.body.heure,
+            nbPlace : req.body.nbPlace,
+            numTable : tableAReserve[0].numero,
+            id_resto: req.params.id_resto,
+            id_planTable: req.params.id_plantable,
+
+        });
+
+        res.status(201).json(`Reservation réussie : ${reservation.email}`);
 
     } catch (error) {
         res.status(500).json({message: "Erreur lors du traitement des données."});
