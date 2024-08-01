8342f717d7d9083ab410a937c9e8747d6e8290df
SarahOtmane <sarahotmane02@gmail.com>
2024-08-01 12:36:05 +0200

    connect to bdd

---
+++ src/services/ConnexionDB.js
@@ -0,0 +1,18 @@
+const sequelize = require("sequelize"); //orm node js pour les bases de données relationnelles
+
+export default function Connexion(){
+    // Configuration de la base de données
+    const db = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: "db",
+        dialect: "mysql"
+    });
+    
+    // Test de la connexion à la base de données
+    db.authenticate()
+        .then(() => {
+            console.log("Connecté à la base de données MySQL!");
+        })
+        .catch(err => {
+            console.error("Impossible de se connecter à la base de données:", err);
+    });
+}
